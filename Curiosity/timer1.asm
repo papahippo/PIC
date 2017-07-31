cb4d11736a33e593c7dada84d098ccecf7fb5be4
papahippo <hippos@chello.nl>
2017-07-31 19:53:25 +0200

    minor changes to allow for code spread over multiple pages (TOSH used).

--- Curiosity/timer1.asm
+++ Curiosity/timer1.asm
@@ -2,13 +2,13 @@
  
        ; general purpose memory usage:
     cblock 0x70                         ;shared memory location that is accessible from all banks
-    saveTOSH
-    saveTOSL
+    Timer1_TOSH
+    Timer1_TOSL
      endc
 
 Timer1 CODE                      ; let linker place this
 
-    global Timer1_Init, Timer1_Handle, Timer1_Await
+    global Timer1_Init, Timer1_IRQ, OnNext_Timer1_IRQ
 Timer1_Init:
     banksel T1CON                  ; select SFR bank	
     movlw   b'00110000'            ; 1:8 prescale, 100mS rollover
@@ -25,10 +25,10 @@ Timer1_Init:
     bsf	    PIE1,TMR1IE            ; enable Timer1 interrupt
 ; The followng call is just a long-stop in-case no-one else claimms timer1
 ; before the interrupt occurs. In that case the following return is harmless.
-    call    Timer1_Await
+    call    OnNext_Timer1_IRQ
     return                         ; return from subroutine
      
-Timer1_Handle:
+Timer1_IRQ:
     banksel PIE1                   ; select SFR bank
     btfss   PIE1,TMR1IE            ; test if interrupt is enabled
     return
@@ -48,14 +48,18 @@ Timer1_Handle:
     bcf	    PIR1,TMR1IF              ; clear Timer1 H/W flag
     bsf	    T1CON,TMR1ON             ; turn on Timer1 module
 
-    movf   saveTOSL,w
+    movf   Timer1_TOSH,w
+    movwf  PCLATH
+    movf   Timer1_TOSL,w
     movwf  PCL
 
-Timer1_Await:
+OnNext_Timer1_IRQ:
      banksel TOSL
      movf	TOSL,w
+     movwf	Timer1_TOSL
+     movf	TOSH,w
+     movwf	Timer1_TOSH
      decf	STKPTR
-     movwf	saveTOSL
      return
 
     END
